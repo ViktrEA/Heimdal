ba315f48d16e95011b9b2c0653117f5584e64ba8
Rodros <rodrigodiazsempere@hotmail.com>
2020-04-17 06:04:23 +0200

    Cambios en front
    Añadidas cards de proyectos y listado de recursos, aún sin vincular con la base de datos porque con la nueva estructura de Angular me he perdido un poco

--- Heimdal/Heimdal-front/src/app/pages/proyectos/proyectos.component.ts
+++ Heimdal/Heimdal-front/src/app/pages/proyectos/proyectos.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class ProyectosComponent implements OnInit {
+  
+  /*proyectos = [{nombre: "proyecto 1", descripcion_corta: "proyecto 1", codigo: "001"}, 
+  {nombre: "proyecto 2", descripcion_corta: "proyecto 2", codigo: "002"}, 
+  {nombre: "proyecto 3", descripcion_corta: "proyecto 3", codigo: "003"}]*/
 
   constructor() { }
 
